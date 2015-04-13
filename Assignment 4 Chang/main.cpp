35bf25a56bc83a1dd72d9ce688fce8819f167004
Susan Chang <susanchang@Susans-Air.home>
2015-04-13 19:19:59 -0400

    Initial Commit

---
+++ Assignment 4 Chang/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 4 Chang
+//
+//  Created by Susan Chang on 4/13/15.
+//  Copyright (c) 2015 Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
